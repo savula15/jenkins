89eaf49ff39f82d5e5d842cf8745a8b5dbdad468
sankar avula <sankaraa@vmware.com>
2018-07-17 02:20:56 -0700

    modified import

--- tests/authentication_tests.py
+++ tests/authentication_tests.py
@@ -3,7 +3,7 @@
 
 from unittest import TestCase
 from mock import patch
-import jenkins.authentication as auth
+import authentication as auth
 
 class StandAloneTests(TestCase):
     """Test the stand-alone module functions."""
